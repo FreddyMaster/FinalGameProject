f5e3b54a7874c2771e204f3db97b33f8092e9e6c
FreddyMaster <blackcatshd@gmail.com>
2024-12-16 00:45:11 -0500

    final fixes

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,21 +4,12 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Reference to the enemy prefab
     public GameObject enemyPrefab;
-
-    // Reference to the health potion prefab
     public GameObject healthPotionPrefab;
-
-    // Reference to the room bounds
     private PolygonCollider2D roomBounds;
-
-    // Reference to the tilemap
     public GameObject doors;
-
-    // Counter to keep track of the number of enemies in the room
+    public Transform pointPosition;
     private int numEnemiesInRoom = 0;
-
     private bool playerInRoom = false;
 
     private void Start()
@@ -30,7 +21,6 @@ public class EnemySpawner : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Player entered a room.");
-        // Check if the player has entered a room
         if (other.gameObject.tag == "Player")
         {
             Debug.Log("Player is in room.");
@@ -43,25 +33,20 @@ public class EnemySpawner : MonoBehaviour
     private void SpawnEnemy()
     {
         Debug.Log("Spawning enemy in room.");
-        // Check if the player is in the room before spawning an enemy
         if (playerInRoom)
         {
             Debug.Log("Player is in room, spawning enemy.");
-            // Calculate a random position within the room bounds
             Vector3 randomPosition = new Vector3(
                 Random.Range(roomBounds.bounds.min.x, roomBounds.bounds.max.x),
                 Random.Range(roomBounds.bounds.min.y, roomBounds.bounds.max.y),
                 0);
 
-            // Instantiate the enemy at the random position
             GameObject enemy = Instantiate(enemyPrefab, randomPosition, Quaternion.identity);
             Debug.Log("Enemy instantiated at position " + randomPosition);
 
-            // Increment the number of enemies in the room
             numEnemiesInRoom++;
             Debug.Log("Number of enemies in room: " + numEnemiesInRoom);
 
-            // Add a listener to the enemy's death event
             EnemyHealth enemyHealth = enemy.GetComponent<EnemyHealth>();
             if (enemyHealth != null)
             {
@@ -82,28 +67,17 @@ public class EnemySpawner : MonoBehaviour
     private void EnemyDeath()
     {
         Debug.Log("Enemy died.");
-        // Decrement the number of enemies in the room
         numEnemiesInRoom--;
         Debug.Log("Number of enemies in room: " + numEnemiesInRoom);
 
-        // Disable the tilemap if all enemies in the room are defeated
         if (numEnemiesInRoom == 0)
         {
-            // Place a health potion in the center of the room
-            Vector3 centerPosition = new Vector3(
-                roomBounds.bounds.center.x,
-                roomBounds.bounds.center.y,
-                0);
-            GameObject healthPotion = Instantiate(healthPotionPrefab, centerPosition, Quaternion.identity);
-            Debug.Log("Placed health potion at center position " + centerPosition);
+            GameObject healthPotion = Instantiate(healthPotionPrefab, pointPosition.transform.position, Quaternion.identity);
+            Debug.Log("Placed health potion at point position " + pointPosition);
 
             doors.gameObject.SetActive(false);
-
-            // Disable colliders once the room is cleared
             GetComponent<PolygonCollider2D>().enabled = false;
         }
     }
 }
-
-
 
